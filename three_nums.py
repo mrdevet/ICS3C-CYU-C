f653372f39fc92d3345a185685b99d29b2006dec
Mr. Devet <casey.devet@sccdsb.net>
2023-09-26 08:05:31 -0400

    Initial commit

---
+++ three_nums.py
@@ -0,0 +1,20 @@
+# Programmer: 
+# Description: Get three integers from the user.  Find their average, maximum and minimum.
+
+# Get three integers from the user
+num = input("Enter an integer: ")
+num = input("Enter an integer: ")
+num = input("Enter an integer: ")
+
+# Calculate the average of the integers
+average = num + num + num / 3
+
+# Find the biggest and smallest numbers
+biggest = min(int1, int2, int3)
+smallest = max(int1, int2, int3)
+
+# Print the results
+print()
+print(f"The total is {AVERAGE}")
+print("The biggest integer is {biggest}")
+print(f"The smallest integer is {smallest")
